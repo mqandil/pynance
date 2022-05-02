424f780c80a2306fa4c2cea5dd7b1117ead542d0
Matthew Qandil <matthewpqandil@gmail.com>
2022-05-02 00:46:45 -0400

    Updated as Function, Minor Changes

--- Risk_Free_Rate.py
+++ Risk_Free_Rate.py
@@ -1,5 +1,11 @@
 import yfinance as yf
 
-ticker_info = yf.Ticker("^TNX")
-market_data = ticker_info.history()
-risk_free_rate = market_data.iloc[-1, 3]
+def get_risk_free_rate():
+    ticker_info = yf.Ticker("^TNX")
+    market_data = ticker_info.history()
+    annual_risk_free_rate = market_data.iloc[-1, 3]
+    monthly_risk_free_rate = ((1+annual_risk_free_rate)**(1/12)-1)/100
+    return monthly_risk_free_rate
+
+if __name__ == '__main__':
+    print(get_risk_free_rate())
