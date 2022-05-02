1c2ea1521f0982c1fa705e4f71d2d1d69db39432
Matthew Qandil <matthewpqandil@gmail.com>
2022-05-02 00:45:49 -0400

    Updated as Function, Minor Changes

--- Data_Aggregator.py
+++ Data_Aggregator.py
@@ -1,28 +1,31 @@
 from Data_Retrieval import dataretriever as dr
 import pandas as pd
 
-ticker_list = ["NKE", "PEP", "SBUX", "TWTR", "ADDYY"]
+ticker_list = ["XOM", "SHW", "JPM", "AEP", "UNH", "AMZN", 
+  "KO", "BA", "AMT", "DD", "TSN", "SLG"]
 
-ticker_data_list = []
-data_count = 0
-ticker_data_indexes = []
+def return_aggregator(ticker_list=ticker_list):
+    ticker_data_list = []
+    data_count = 0
+    ticker_data_indexes = []
 
-for ticker in ticker_list:
-    ticker_data = dr(ticker)
-    ticker_data_list.append(list(ticker_data))
-    data_count = ticker_data.count()
-    ticker_data_indexes = ticker_data.index
+    for ticker in ticker_list:
+        ticker_data = dr(ticker)
+        ticker_data_list.append(list(ticker_data))
+        data_count = ticker_data.count()
+        ticker_data_indexes = ticker_data.index
 
-aggregated_returns = pd.DataFrame(
-    data=ticker_data_list,
-    index=ticker_list,
-    columns=ticker_data_indexes
-)
+    aggregated_returns = pd.DataFrame(
+        data=ticker_data_list,
+        index=ticker_list,
+        columns=ticker_data_indexes
+    )
 
-aggregated_returns = aggregated_returns.transpose()
+    aggregated_returns = aggregated_returns.transpose()
+    return aggregated_returns
 
 if __name__ == "__main__":
-    aggregated_returns
+    return_aggregator()
 
 
     
