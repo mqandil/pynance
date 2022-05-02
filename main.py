adf87af6a9ea3ca3229c12534233dc2836c6eddd
Matthew Qandil <matthewpqandil@gmail.com>
2022-05-02 12:21:06 -0400

    User Interface Begun...

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from Portfolio_Calculator import PortfolioCalculations as pc
 
+ticker_list = [
+        "XOM", "SHW", "JPM", "AEP", "UNH", "AMZN", 
+        "KO", "BA", "AMT", "DD", "TSN", "SLG"
+    ]
+
+print(pc(ticker_list).max_sharpe_portfolio())
+print(pc(ticker_list).min_std_portfolio())
+pc(ticker_list).efficient_frontier()
