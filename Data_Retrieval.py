2dd92b93181b59528d3da7b8cc5a34616b947819
Matthew Qandil <matthewpqandil@gmail.com>
2022-05-02 14:51:00 -0400

    Added TickerError Exception

--- Data_Retrieval.py
+++ Data_Retrieval.py
@@ -11,11 +11,18 @@ def dataretriever(ticker, period = period, interval = interval):
     #select adj close line, drop N/A values, and change to percent change, then drop first column (no data)
     adj_close_data = market_data.iloc[:, 3].dropna().pct_change().dropna()
 
+    if adj_close_data.empty:
+        print(f"'{ticker}' is not a valid ticker.")
+        raise TickerError
+
     #remove last line if not updated to next month
     if adj_close_data[-1] == 0:
         adj_close_data = adj_close_data[0:-1]
 
     return adj_close_data
 
+class TickerError(Exception):
+    pass
+
 if __name__ == "__main__":
-    print(dataretriever())
+    print(dataretriever('HLIS'))
