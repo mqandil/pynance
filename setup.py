f920159350e51c498ee3f051d84c14b7f2ddcac2
Matthew Qandil <matthewpqandil@gmail.com>
2022-05-10 22:48:18 -0400

    Small Changes...

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-if __name__ == '__main__':
-    setup()
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ['pandas>=1.3.2', 'numpy>=1.21.2', 'yfinance>=0.1.70', 'seaborn>=0.11.2', 'matplotlib>=3.4.3', 'scipy>=1.7.1', 'plotly>=5.7.0',]
+
+setup(
+    name="PortfolioOptimizer",
+    version="0.0.2",
+    author="Matthew Qandil",
+    description="A package to Optimize Stock Portfolios",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mqandil/PortfolioOptimizer/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
